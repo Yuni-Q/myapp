06952dec0b5adbda320e4c58da7d81431ecfdd09
Yuni-Q <carnotcycle2@naver.com>
2018-09-17 10:45:06 +0900

    Fix model Users

--- models/user.js
+++ models/user.js
@@ -1,53 +1,17 @@
-// 'use strict';
-// const Sequelize = require('sequelize');
-
-// module.exports = (Sequelize, DataTypes) => {
-//   const User = Sequelize.define('User', {
-//     user_name: DataTypes.STRING,
-//     password: DataTypes.STRING
-//   }, {});
-//   User.associate = function (models) {
-//     // associations can be defined here
-//   };
-//   return User;
-// };
-
+"use strict";
 const Sequelize = require("sequelize");
 
-module.exports = db => {
-  const Users = db.define(
+module.exports = (Sequelize, DataTypes) => {
+  const Users = Sequelize.define(
     "Users",
     {
-      id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      user_name: {
-        type: Sequelize.STRING
-      },
-      password: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        type: Sequelize.DATE,
-        allowNull: true,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
-      },
-      updatedAt: {
-        type: Sequelize.DATE,
-        allowNull: true,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
-      }
+      user_name: DataTypes.STRING,
+      password: DataTypes.STRING
     },
-    {
-      validations: {},
-      methods: {},
-      tableName: "Users",
-      timestamps: false,
-      underscored: false
-    }
+    {}
   );
-
+  Users.associate = function(models) {
+    // associations can be defined here
+  };
   return Users;
 };
