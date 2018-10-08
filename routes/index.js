1f5a930164b8a973169ca9ed14c4cb9386f03974
Yuni-Q <carnotcycle2@naver.com>
2018-10-08 15:45:02 +0900

    Add RxJs

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var router = express.Router();
+const request = require("request");
+const _ = require("lodash");
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
@@ -15,6 +17,55 @@ router.get("/", function(req, res, next) {
   // 쿠키 삭제
   // res.clearCookie(key);
   // res.clearCookie(key, {path:'/path'});
+});
+
+router.get("/movies", async function(req, res, next) {
+  let pre_movies;
+  let movies;
+  let temp = [];
+  let row = {};
+  let size;
+  let result;
+  request("https://yts.ag/api/v2/list_movies.json?limit=50", function(
+    error,
+    response,
+    body
+  ) {
+    console.time();
+    try {
+      if (!error && response.statusCode == 200) {
+        let pre_movies = JSON.parse(body);
+        let movies = pre_movies.data.movies;
+        let temp = [];
+        let row = {};
+        let size;
+        let result;
+
+        movies.forEach(element => {
+          element.torrents.forEach(e => {
+            if (e.size.slice(-2, -1) == "G") {
+              size = e.size.slice(0, -2) * 1024;
+            } else {
+              size = e.size.slice(0, -2) * 1;
+            }
+            title = element.title;
+            quality = e.quality;
+            row = { title, quality, size };
+            temp.push(row);
+          });
+        });
+        result = _.sortBy(temp, ["size"]);
+        result.forEach(e => {
+          e.size = e.size + " MB";
+        });
+        // console.log(result);
+        console.timeEnd();
+        res.json(result);
+      }
+    } catch (_err) {
+      console.log(_err);
+    }
+  });
 });
 
 module.exports = router;
