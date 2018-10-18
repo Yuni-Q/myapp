47f5edda212470e1b5518e9a8c7ba3bffdd5e601
Yuni-Q <carnotcycle2@naver.com>
2018-10-19 00:13:14 +0900

    FIx sequelize

---
+++ models/user.js
@@ -0,0 +1,17 @@
+// "use strict";
+// const Sequelize = require('sequelize');
+
+module.exports = (Sequelize, DataTypes) => {
+  const Users = Sequelize.define(
+    'Users',
+    {
+      user_name: DataTypes.STRING,
+      password: DataTypes.STRING,
+    },
+    {},
+  );
+  // Users.associate = (models) => {
+  //   // associations can be defined here
+  // };
+  return Users;
+};
