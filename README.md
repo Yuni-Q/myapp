53a14676d81887b6dbe3df7206b4f7e91aa5f6a8
Yuni-Q <carnotcycle2@naver.com>
2018-10-22 11:26:33 +0900

    Fix sequelize

--- README.md
+++ README.md
@@ -7,3 +7,14 @@
 ## Swagger
 ## MongoDB
 ## passport
+## slackbot
+
+### sequelize-cli 사용
+```bash
+npm i sequelize -g
+npm i sequelize-cli -g
+npm i mysql2 -g
+sequelize db:create
+sequelize db:migrate
+sequelize db:create:undo
+```
--- config/environment.js
+++ config/environment.js
@@ -60,11 +60,11 @@ module.exports = (app) => {
   // /* =======================
   //  CONNECT TO MONGODB SERVER
   // ==========================*/
-  // mongoose.connect(global.config.mongo.mongodbUri);
-  // mongoose.connect(global.config.mongo.mongodbUri, { useNewUrlParser: true });
-  // const db = mongoose.connection;
-  // db.on('error', console.error);
-  // db.once('open', () => {
-  //   console.log('connected to mongodb server');
-  // });
+  mongoose.connect(global.config.mongo.mongodbUri);
+  mongoose.connect(global.config.mongo.mongodbUri, { useNewUrlParser: true });
+  const db = mongoose.connection;
+  db.on('error', console.error);
+  db.once('open', () => {
+    console.log('connected to mongodb server');
+  });
 };
