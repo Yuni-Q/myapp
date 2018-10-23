e1cda5b7008ba61c1d38a8bdc5e974c5a0efefbd
Yuni-Q <carnotcycle2@naver.com>
2018-10-23 19:02:38 +0900

    Add query

---
+++ models/query.js
@@ -0,0 +1,12 @@
+
+// const Promise = require('bluebird');
+// const url = require('url');
+// const moment = require('moment');
+// const uuid = require('uuid-random');
+// const models = require('../models');
+
+exports.user = {
+  findUser(id) {
+    return id;
+  },
+};
