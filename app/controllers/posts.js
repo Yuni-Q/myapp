fff31f2d1963e1ada1d87d5d15fc1323a790a412
Yuni-Q <carnotcycle2@naver.com>
2018-10-30 21:17:45 +0900

    Fix swagger & url

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -32,8 +32,8 @@ router.get('/create', isLoggedIn, async (req, res) => {
   });
 });
 
-router.get('/edit', isLoggedIn, async (req, res) => {
-  const { _id } = req.query;
+router.get('/:_id/edit', isLoggedIn, async (req, res) => {
+  const { _id } = req.params;
   const posts = await Post.findOne({ _id });
   const date = await moment(posts.date).format('YYYY-MM-DD');
   res.render('./posts/edit', {
@@ -44,8 +44,9 @@ router.get('/edit', isLoggedIn, async (req, res) => {
   });
 });
 
-router.put('/', isLoggedIn, async (req, res) => {
-  const { _id, todo, date } = req.body;
+router.put('/:_id', isLoggedIn, async (req, res) => {
+  const { _id } = req.params;
+  const { todo, date } = req.body;
   const posts = await Post.findOne({ _id });
   posts.todo = todo;
   posts.date = date;
@@ -53,8 +54,8 @@ router.put('/', isLoggedIn, async (req, res) => {
   res.json(result);
 });
 
-router.delete('/', isLoggedIn, async (req, res) => {
-  const { _id } = req.body;
+router.delete('/:_id', isLoggedIn, async (req, res) => {
+  const { _id } = req.params;
   const result = await Post.deleteOne({ _id });
   res.json(result);
 });
